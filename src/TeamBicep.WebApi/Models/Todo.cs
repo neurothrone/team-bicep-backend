a6bf0a41943193a1970f9d75c00f6abce5b42fb2
DilanRaza1 <dilan.raza@iths.se>
2025-09-02 10:26:19 +0200

    Feat: Create mongo models

---
+++ src/TeamBicep.WebApi/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace TeamBicep.WebApi.Models;
+
+public class Todo
+{
+    [BsonElement("Id")]
+    public string Id { get; set; } = string.Empty;
+
+    [BsonElement("Name")]
+    public string Name { get; set; } = string.Empty;
+    [BsonElememt("IsCompleted")]
+    public bool IsCompleted { get; set; } = false;
+
+}
