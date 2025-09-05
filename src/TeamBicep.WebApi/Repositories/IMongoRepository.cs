24532a890d3506366a49c3db79846957f9b2c557
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-09-05 10:04:26 +0200

    remove redundant mongo repository and interface

--- src/TeamBicep.WebApi/Repositories/IMongoRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TeamBicep.WebApi.Repositories;
-
-
-public interface IMongoRepository<TEntity>
-{
-    Task<List<TEntity>> GetAllAsync();
-    Task<TEntity> GetByIdAsync(string id);
-    Task AddAsync(TEntity entity);
-}
